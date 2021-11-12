15f9ddda0d445db5022c365f7b826b25380d5e02
BrainLess <71016915+Aksheet10@users.noreply.github.com>
2021-11-12 11:12:17 +0530

    Update answerer.py

--- answerer.py
+++ answerer.py
@@ -21,7 +21,7 @@ def webopen(url):
         "chrome",
         None,
         webbrowser.BackgroundBrowser(
-            "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
+            "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
         ),
     )
     webbrowser.get("chrome").open(url)
